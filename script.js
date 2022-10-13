67ba0932883affbf95576d1e361a8486a2f078ef
roberttmello <roberttmello@protonmail.com>
2022-10-13 17:34:21 -0300

    Finalizado parte principal

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 // Selecionando e guardando a referência para o canvas criado no html
-const canvas = document.getElementById("canvas1");
-// Criando contexto(2d) para acessar as respectivas funções do canvas
-const context = canvas.getContext("2d");
+const CANVAS = document.getElementById("canvas1");
+
+// Criando o contexto(2d) para acessar as respectivas funções do canvas
+const CONTEXT = CANVAS.getContext("2d");
 
 // Setando o tamanho do canvas
-const CANVAS_WIDTH = (canvas.width = 600);
-const CANVAS_HEIGHT = (canvas.height = 600);
+const CANVAS_WIDTH = (CANVAS.width = 600);
+const CANVAS_HEIGHT = (CANVAS.height = 600);
 
 // Usando a classe Image do javascript para carregar uma imagem
-const playerImage = new Image();
-playerImage.src = "../assets/images/shadow_dog.png";
+const PLAYER_IMAGE = new Image();
+PLAYER_IMAGE.src = "../assets/images/shadow_dog.png";
 
+// Tamanho de cada sprite
 const SPRITE_WIDTH = 575;
 const SPRITE_HEIGHT = 523;
 
-let frameX = 0;
-let frameY = 0;
-let gameFrame = 0;
-const STAGGER_FRAMES = 4;
+const SPRITE_ANIMATIONS = {
+  idle: { animationId: 0, quantFrames: 6 },
+  jump: { animationId: 1, quantFrames: 6 },
+  fall: { animationId: 2, quantFrames: 6 },
+  run: { animationId: 3, quantFrames: 8 },
+  dizzy: { animationId: 4, quantFrames: 10 },
+  sit: { animationId: 5, quantFrames: 4 },
+  roll: { animationId: 6, quantFrames: 6 },
+  bite: { animationId: 7, quantFrames: 6 },
+  ko: { animationId: 8, quantFrames: 11 },
+  getHit: { animationId: 9, quantFrames: 3 },
+};
 
-const spriteAnimations = [
-  { name: "idle", frames: 7 },
-  { name: "jump", frames: 7 },
-];
+// Constante que controla a velocidade da animação.
+const ANIMATION_VELOCITY = 4;
+
+let animation = "run";
+let frameX = 0;
+let gameFrame = 0;
+let animationID = SPRITE_ANIMATIONS[animation]["animationId"];
 
 function animateSprite() {
   /**
    *"clearRect" limpa todos os pixels do nosso canvas a cada frame executado.
     Seus parâmetros são as coordenadas(x,y) do ponto inicial do retângulo e lagura e altura do mesmo. 
   */
-  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  CONTEXT.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
   // drawImage(image, sourceX, sourceY, sourceW, sourceH, destX, destY, destW, destH);
   /**
@@ -39,10 +52,10 @@ function animateSprite() {
    * "destX, destY, destW, destH" -> Determina em qual parte do nosso canvas iremos mostrar a imagem cortada. 
    */
 
-  context.drawImage(
-    playerImage,
+  CONTEXT.drawImage(
+    PLAYER_IMAGE,
     frameX * SPRITE_WIDTH,
-    frameY * SPRITE_HEIGHT,
+    animationID * SPRITE_HEIGHT,
     SPRITE_WIDTH,
     SPRITE_HEIGHT,
     0,
@@ -51,10 +64,12 @@ function animateSprite() {
     SPRITE_HEIGHT
   );
 
-  if (gameFrame % STAGGER_FRAMES === 0) {
-    if (frameX < 6) frameX++;
+  if (gameFrame % ANIMATION_VELOCITY === 0) {
+    if (frameX < SPRITE_ANIMATIONS[animation]["quantFrames"]) frameX++;
     else frameX = 0;
   }
+
+  // "gameFrame" é incrementado a cada frame da animação.
   gameFrame++;
 
   /**
