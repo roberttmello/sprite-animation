878858ac3f75db01f72b2c755dd0116f50acbedc
roberttmello <roberttmello@protonmail.com>
2022-10-18 13:12:31 -0300

    Prettier config changes

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 // Variável que controla qual animação será executada
-let animation = "idle";
+let animation = 'idle'
 
-let animationID = null;
+let animationID = null
 
 // Função que altera a variável "animation" quando
 // o evento "change" do select é identificado.
 const setAnimation = (event) => {
-  animation = event.target.value;
-  animationID = SPRITE_ANIMATIONS[animation]["animationId"];
-};
+  animation = event.target.value
+  animationID = SPRITE_ANIMATIONS[animation]['animationId']
+}
 
 // Selecionando o elemento "select" do html
-const SELECT_ELEMENT = document.querySelector("select");
+const SELECT_ELEMENT = document.querySelector('select')
 
 // Monitorando o evento "change" do elemento "select"
-SELECT_ELEMENT.addEventListener("change", setAnimation);
+SELECT_ELEMENT.addEventListener('change', setAnimation)
 
 // Selecionando e guardando a referência para o canvas criado no html
-const CANVAS = document.getElementById("canvas1");
+const CANVAS = document.getElementById('canvas1')
 
 // Criando o contexto(2d) para acessar as respectivas funções do canvas
-const CONTEXT = CANVAS.getContext("2d");
+const CONTEXT = CANVAS.getContext('2d')
 
 // Setando o tamanho do canvas
-const CANVAS_WIDTH = (CANVAS.width = 600);
-const CANVAS_HEIGHT = (CANVAS.height = 600);
+const CANVAS_WIDTH = (CANVAS.width = 600)
+const CANVAS_HEIGHT = (CANVAS.height = 600)
 
 // Usando a classe Image do javascript para carregar uma imagem
-const PLAYER_IMAGE = new Image();
-PLAYER_IMAGE.src = "../assets/images/shadow_dog.png";
+const PLAYER_IMAGE = new Image()
+PLAYER_IMAGE.src = '../assets/images/shadow_dog.png'
 
 // Tamanho de cada sprite
-const SPRITE_WIDTH = 575;
-const SPRITE_HEIGHT = 523;
+const SPRITE_WIDTH = 575
+const SPRITE_HEIGHT = 523
 
 const SPRITE_ANIMATIONS = {
   idle: { animationId: 0, quantFrames: 6 },
@@ -45,20 +45,20 @@ const SPRITE_ANIMATIONS = {
   bite: { animationId: 7, quantFrames: 6 },
   ko: { animationId: 8, quantFrames: 11 },
   getHit: { animationId: 9, quantFrames: 3 },
-};
+}
 
 // Constante que controla a velocidade da animação.
-const ANIMATION_VELOCITY = 4;
+const ANIMATION_VELOCITY = 4
 
-let frameX = 0;
-let gameFrame = 0;
+let frameX = 0
+let gameFrame = 0
 
 function animateSprite() {
   /**
    *"clearRect" limpa todos os pixels do nosso canvas a cada frame executado.
     Seus parâmetros são as coordenadas(x,y) do ponto inicial do retângulo e lagura e altura do mesmo. 
   */
-  CONTEXT.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  CONTEXT.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
   // drawImage(image, sourceX, sourceY, sourceW, sourceH, destX, destY, destW, destH);
   /**
@@ -78,20 +78,20 @@ function animateSprite() {
     0,
     SPRITE_WIDTH,
     SPRITE_HEIGHT
-  );
+  )
 
   if (gameFrame % ANIMATION_VELOCITY === 0) {
-    if (frameX < SPRITE_ANIMATIONS[animation]["quantFrames"]) frameX++;
-    else frameX = 0;
+    if (frameX < SPRITE_ANIMATIONS[animation]['quantFrames']) frameX++
+    else frameX = 0
   }
 
   // "gameFrame" é incrementado a cada frame da animação.
-  gameFrame++;
+  gameFrame++
 
   /**
    *"requestAnimationFrame" executa a função passada como argumento(animateSprite)
      para cada frame da nossa tela(60fps geralmente). Logo, aqui temos nosso loop da nossa animação. 
    */
-  requestAnimationFrame(animateSprite);
+  requestAnimationFrame(animateSprite)
 }
-animateSprite();
+animateSprite()
